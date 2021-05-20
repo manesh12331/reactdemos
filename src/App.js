c7e6400b2d0a57b257ba942f97f6767bf5133ef9
manesh <maneshpunde69@gmail.com>
2021-05-20 20:26:30 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstExamle from './components/FirstExamle';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <FirstExamle></FirstExamle>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FirstExamle.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FirstExamle extends Component {
+    render() {
+        return (
+            <div>
+                <h1>first Example</h1>
+            </div>
+        );
+    }
+}
+
+export default FirstExamle;
